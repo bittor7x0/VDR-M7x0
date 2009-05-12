d53930ad05f8c3781d40cb3e34236f5900f09b62
bittor <bittor@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2009-05-12 18:50:58 +0000

    Añadidos los programas dbench, tbench y unixbench para testear el rendimiento del M7x0. No se incluyen en el firmware debido a que tienen que ser ejecutados desde un disco duro (por ejemplo dbench y tbench necesitan un archivo de 25 MiB), pero después de compilar tendremos un archivo comprimido (en el mismo directorio donde se genera el firmware ofi o wsw) para cada programa con todo lo necesario para hacer las pruebas. Por defecto están desactivados y hay que activarlos en el .config.
    · dbench sirve para hacer tests de disco duro permitiendo hacer la simulación de varios clientes concurrentes.
    · tbench testea la red generando tráfico TCP y carga de procesos (no hace llamadas al sistema de archivos).
    · unixbench es un completo programa que hace bastantes testeos como carga del sistema, disco duro, pipe, CPU, etc.

---
+++ unstable/Addons/Files/common/make-incs/dbench.mk
@@ -0,0 +1,122 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# The project's page is at http://www.open7x0.org
+#
+#
+
+# Put dependencies here all pack should depend on $$(BASE_BUILD_STAGEFILE)
+DBENCH_DEPS = $(BASE_BUILD_STAGEFILE)
+
+DBENCH_VERSION := 3.04
+DBENCH_PATCHES_DIR := $(PATCHES_DIR)/dbench/$(DBENCH_VERSION)
+
+#http://samba.org/ftp/tridge/dbench/dbench-3.04.tar.gz
+DBENCH_FILE := dbench-$(DBENCH_VERSION).tar.gz
+DBENCH_DLFILE := $(DOWNLOAD_DIR)/$(DBENCH_FILE)
+DBENCH_URL := http://samba.org/ftp/tridge/dbench/$(DBENCH_FILE)
+DBENCH_DIR := $(BUILD_DIR)/dbench-$(DBENCH_VERSION)
+
+DBENCH_PACKED = $(STAGEFILES_DIR)/.dbench_packed
+
+PACKS_RULES_$(CONFIG_DBENCH) += $(DBENCH_PACKED)
+
+CLEAN_RULES += clean-dbench
+DISTCLEAN_RULES += distclean-dbench
+
+#
+# download dbench
+#
+
+$(DBENCH_DLFILE): $(TC_INIT_RULE)
+	(if [ ! -f $(DBENCH_DLFILE) ] ; then \
+	$(WGET) $(DBENCH_URL) -O $(DBENCH_DLFILE) ; \
+	fi );
+	$(TOUCH) $(DBENCH_DLFILE)
+
+#
+# unpack dbench
+#
+
+$(STAGEFILES_DIR)/.dbench_unpacked: $(DBENCH_DLFILE) \
+                                           $(wildcard $(DBENCH_PATCHES_DIR)/*.patch) \
+                                           $$(DBENCH_DEPS)
+	-$(RM) -rf $(DBENCH_DIR)
+	$(TAR) -C $(BUILD_DIR) -zf $(DBENCH_DLFILE)
+	$(TOUCH) $(STAGEFILES_DIR)/.dbench_unpacked
+
+#
+# patch dbench
+#
+
+$(STAGEFILES_DIR)/.dbench_patched: $(STAGEFILES_DIR)/.dbench_unpacked
+	$(call patch_package, $(DBENCH_DIR), $(DBENCH_PATCHES_DIR))
+	$(TOUCH) $(STAGEFILES_DIR)/.dbench_patched
+
+#
+# configure dbench
+#
+
+$(STAGEFILES_DIR)/.dbench_configured: $(STAGEFILES_DIR)/.dbench_patched
+	($(CD) $(DBENCH_DIR) ; $(DBENCH_DIR)/autogen.sh ; \
+		$(UCLIBC_ENV) $(DBENCH_DIR)/configure \
+			--prefix=$(TARGET_ROOT)/usr \
+			--host=$(TARGET))
+	$(TOUCH) $(STAGEFILES_DIR)/.dbench_configured
+
+#
+# compile dbench
+#
+
+$(STAGEFILES_DIR)/.dbench_compiled: $(STAGEFILES_DIR)/.dbench_configured
+	$(UCLIBC_ENV) $(MAKE) \
+		-C $(DBENCH_DIR)
+	$(TOUCH) $(STAGEFILES_DIR)/.dbench_compiled
+
+#
+# pack dbench
+#
+
+$(STAGEFILES_DIR)/.dbench_packed: $(STAGEFILES_DIR)/.dbench_compiled
+	$(PREFIX_BIN)/$(UCLIBC_STRIP) $(DBENCH_DIR)/dbench
+	$(PREFIX_BIN)/$(UCLIBC_STRIP) $(DBENCH_DIR)/tbench
+	$(PREFIX_BIN)/$(UCLIBC_STRIP) $(DBENCH_DIR)/tbench_srv
+	-$(RM) -f $(TOP_DIR)/dbench-$(DBENCH_VERSION)-m7x0.tar.bz2
+	tar jcvf $(TOP_DIR)/dbench-$(DBENCH_VERSION)-m7x0.tar.bz2 \
+		--directory=$(BUILD_DIR) \
+		dbench-$(DBENCH_VERSION)/client.txt \
+		dbench-$(DBENCH_VERSION)/dbench \
+		dbench-$(DBENCH_VERSION)/README \
+		dbench-$(DBENCH_VERSION)/tbench \
+		dbench-$(DBENCH_VERSION)/tbench_srv
+	$(TOUCH) $(STAGEFILES_DIR)/.dbench_packed
+
+.PHONY: clean-dbench distclean-dbench
+
+clean-dbench:
+	-$(RM) -rf $(DBENCH_DIR)
+
+#
+# clean everthing else
+#
+
+distclean-dbench:
+	-$(RM) -f $(STAGEFILES_DIR)/.dbench_unpacked
+	-$(RM) -f $(STAGEFILES_DIR)/.dbench_patched
+	-$(RM) -f $(STAGEFILES_DIR)/.dbench_configured
+	-$(RM) -f $(STAGEFILES_DIR)/.dbench_compiled
+	-$(RM) -f $(STAGEFILES_DIR)/.dbench_packed
+ifeq ($(DISTCLEAN_DLFILE),y)
+	-$(RM) -rf $(DBENCH_DLFILE)
+endif
---
+++ unstable/Addons/Files/common/make-incs/unixbench.mk
@@ -0,0 +1,118 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# The project's page is at http://www.open7x0.org
+#
+#
+
+# Put dependencies here all pack should depend on $$(BASE_BUILD_STAGEFILE)
+UNIXBENCH_DEPS = $(BASE_BUILD_STAGEFILE)
+
+UNIXBENCH_VERSION := 4.1.0
+UNIXBENCH_PATCHES_DIR := $(PATCHES_DIR)/unixbench/$(UNIXBENCH_VERSION)
+
+#http://www.tux.org/pub/tux/niemi/unixbench/unixbench-4.1.0.tgz
+UNIXBENCH_FILE := unixbench-$(UNIXBENCH_VERSION).tgz
+UNIXBENCH_DLFILE := $(DOWNLOAD_DIR)/$(UNIXBENCH_FILE)
+UNIXBENCH_URL := http://www.tux.org/pub/tux/niemi/unixbench/$(UNIXBENCH_FILE)
+UNIXBENCH_DIR := $(BUILD_DIR)/unixbench-$(UNIXBENCH_VERSION)
+
+UNIXBENCH_PACKED = $(STAGEFILES_DIR)/.unixbench_packed
+
+PACKS_RULES_$(CONFIG_UNIXBENCH) += $(UNIXBENCH_PACKED)
+
+CLEAN_RULES += clean-unixbench
+DISTCLEAN_RULES += distclean-unixbench
+
+#
+# download unixbench
+#
+
+$(UNIXBENCH_DLFILE): $(TC_INIT_RULE)
+	(if [ ! -f $(UNIXBENCH_DLFILE) ] ; then \
+	$(WGET) $(UNIXBENCH_URL) -O $(UNIXBENCH_DLFILE) ; \
+	fi );
+	$(TOUCH) $(UNIXBENCH_DLFILE)
+
+#
+# unpack unixbench
+#
+
+$(STAGEFILES_DIR)/.unixbench_unpacked: $(UNIXBENCH_DLFILE) \
+                                           $(wildcard $(UNIXBENCH_PATCHES_DIR)/*.patch) \
+                                           $$(UNIXBENCH_DEPS)
+	-$(RM) -rf $(UNIXBENCH_DIR)
+	$(TAR) -C $(BUILD_DIR) -zf $(UNIXBENCH_DLFILE)
+	$(TOUCH) $(STAGEFILES_DIR)/.unixbench_unpacked
+
+#
+# patch unixbench
+#
+
+$(STAGEFILES_DIR)/.unixbench_patched: $(STAGEFILES_DIR)/.unixbench_unpacked
+	$(call patch_package, $(UNIXBENCH_DIR), $(UNIXBENCH_PATCHES_DIR))
+	$(TOUCH) $(STAGEFILES_DIR)/.unixbench_patched
+
+#
+# compile unixbench
+#
+
+$(STAGEFILES_DIR)/.unixbench_compiled: $(STAGEFILES_DIR)/.unixbench_patched
+	$(UCLIBC_ENV) $(MAKE) \
+		-C $(UNIXBENCH_DIR)
+	$(TOUCH) $(STAGEFILES_DIR)/.unixbench_compiled
+
+#
+# strip unixbench
+#
+
+$(STAGEFILES_DIR)/.unixbench_stripped: $(STAGEFILES_DIR)/.unixbench_compiled
+	$(UCLIBC_ENV) $(MAKE) strip \
+		-C $(UNIXBENCH_DIR)
+	$(TOUCH) $(STAGEFILES_DIR)/.unixbench_stripped
+
+#
+# pack unixbench
+#
+
+$(STAGEFILES_DIR)/.unixbench_packed: $(STAGEFILES_DIR)/.unixbench_stripped
+	-$(RM) -f $(TOP_DIR)/unixbench-$(UNIXBENCH_VERSION)-m7x0.tar.bz2
+	tar jcvf $(TOP_DIR)/unixbench-$(UNIXBENCH_VERSION)-m7x0.tar.bz2 \
+		--directory=$(BUILD_DIR) \
+		unixbench-$(UNIXBENCH_VERSION)/pgms/ \
+		unixbench-$(UNIXBENCH_VERSION)/results/ \
+		unixbench-$(UNIXBENCH_VERSION)/testdir/ \
+		unixbench-$(UNIXBENCH_VERSION)/tmp/ \
+		unixbench-$(UNIXBENCH_VERSION)/README \
+		unixbench-$(UNIXBENCH_VERSION)/Run
+	$(TOUCH) $(STAGEFILES_DIR)/.unixbench_packed
+
+.PHONY: clean-unixbench distclean-unixbench
+
+clean-unixbench:
+	-$(RM) -rf $(UNIXBENCH_DIR)
+
+#
+# clean everthing else
+#
+
+distclean-unixbench:
+	-$(RM) -f $(STAGEFILES_DIR)/.unixbench_unpacked
+	-$(RM) -f $(STAGEFILES_DIR)/.unixbench_patched
+	-$(RM) -f $(STAGEFILES_DIR)/.unixbench_compiled
+	-$(RM) -f $(STAGEFILES_DIR)/.unixbench_stripped
+	-$(RM) -f $(STAGEFILES_DIR)/.unixbench_packed
+ifeq ($(DISTCLEAN_DLFILE),y)
+	-$(RM) -rf $(UNIXBENCH_DLFILE)
+endif
