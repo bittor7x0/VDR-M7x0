5e375928a5cf456ba0854cb462a470476dadcafc
bittor <bittor@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-11 10:08:14 +0000

    Actualizar a la última versión GIT del EPGSearch

---
+++ unstable/VDR-Plugins/Source+patch/epgsearch-git-2010-06-06/epgsearch-git-2010-06-06-orig/scripts/epg2taste.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# epg2taste.sh - v.0.1
+#
+# add this line to your epgsearchcmds.conf:
+# folgende zeile in die epgsearchcmds.conf eintragen:
+#
+# epg2taste : /usr/local/bin/epg2taste.sh
+
+# CONFIG START
+  TASTE_FILE="/etc/vdr/plugins/taste.conf"
+
+# default taste settings
+  REGULAR_EXPRESSION=0 # Regular Expression / Regulärer Ausdruck
+  IGNORE_CASE=0        # Ignore Case / Groß/Kleinschreibung ignorieren
+# CONFIG END
+
+# add taste
+echo "${REGULAR_EXPRESSION}:${IGNORE_CASE}:${1}" >> "${TASTE_FILE}"
+echo "Done..."
+
+# jump back
+at now <<EOF
+perl -l -e "printf \"\n/usr/local/bin/svdrpsend.pl HITK BACK\" x 2" | sh
+EOF
+
