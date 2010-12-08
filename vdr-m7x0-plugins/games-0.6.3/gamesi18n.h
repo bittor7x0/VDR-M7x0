55c05ae214943dc04c46f1f9db23fc0f060fb435
bittor <76845083+bittor7x0@users.noreply.github.com>
2010-12-08 11:24:23 +0000

    Adaptar plugin games-0.6.3 para compilar en M7x0 y añadir traducción al español.

---
+++ vdr-m7x0-plugins/games-0.6.3/gamesi18n.h
@@ -0,0 +1,227 @@
+/*
+ * i18n.h: A plugin for the Video Disk Recorder
+ *
+ * See the README file for copyright information and how to reach the author.
+ *
+ * $Id: i18n.h,v 1.1 2003/11/11 15:06:58 opj Exp $
+ */
+
+#ifndef __GAMES_I18N_H
+#define __GAMES_I18N_H
+
+#include <config.h> // only for VDRVERSNUM
+#include <i18n.h>
+
+const tI18nPhrase Phrases[] =
+{
+  { "OSD Games Collection", // English
+    "", // Deutsch
+    "", // Slovenski
+    "", // Italiano
+    "", // Nederlands
+    "", // Português
+    "", // Français
+    "", // Norsk
+    "Pelit (OSD)", // suomi
+    "", // Polski
+    "Colección Juegos OSD", // Español
+    "", // ÅëëçíéêÜ (Greek)
+    "", // Svenska
+    "", // Romaneste
+    "", // Magyar
+    "", // Català
+#if VDRVERSNUM >= 10302
+    "", // ÀãááÚØÙ (Russian)
+#if VDRVERSNUM >= 10307
+    "", // Hrvatski (Croatian)
+#endif
+#endif
+  },
+  { "Games", // English
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "Pelit", // suomi
+    "", // TODO
+    "Juegos", // Español
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+#if VDRVERSNUM >= 10302
+    "", // TODO
+#if VDRVERSNUM >= 10307
+    "", // TODO
+#endif
+#endif
+  },
+  { "X Position", // English
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "Vaakasijainti", // suomi
+    "", // TODO
+    "Posición X", // Español
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+#if VDRVERSNUM >= 10302  
+    "", // TODO               
+#if VDRVERSNUM >= 10307  
+    "", // TODO                 
+#endif  
+#endif  
+  },
+  { "Y Position", // English
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "Pystysijainti", // suomi
+    "", // TODO
+    "Posición Y", // Español
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+#if VDRVERSNUM >= 10302  
+    "", // TODO               
+#if VDRVERSNUM >= 10307  
+    "", // TODO                 
+#endif  
+#endif  
+  },
+  { "Computer Skill", // English
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "Vaikeustaso", // suomi
+    "", // TODO
+    "Habilidad del ordenador", // Español
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+#if VDRVERSNUM >= 10302  
+    "", // TODO               
+#if VDRVERSNUM >= 10307  
+    "", // TODO                 
+#endif  
+#endif  
+  },
+  { "Key Repeat", // English
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "Näppäimen toisto", // suomi
+    "", // TODO
+    "Botón repetir", // Español
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+#if VDRVERSNUM >= 10302  
+    "", // TODO               
+#if VDRVERSNUM >= 10307  
+    "", // TODO                 
+#endif  
+#endif  
+  },
+  { "OSD Size", // English
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "Näytön koko", // suomi
+    "", // TODO
+    "Tamaño OSD", // Español
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+#if VDRVERSNUM >= 10302  
+    "", // TODO               
+#if VDRVERSNUM >= 10307  
+    "", // TODO                 
+#endif  
+#endif  
+  },
+  { "On", // English
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "päällä", // suomi
+    "", // TODO
+    "Activar", // Español
+    "", // TODO
+    "", // TODO  
+    "", // TODO   
+    "", // TODO
+    "", // TODO   
+#if VDRVERSNUM >= 10302  
+    "", // TODO               
+#if VDRVERSNUM >= 10307  
+    "", // TODO                 
+#endif  
+#endif  
+  },
+  { "Off", // English
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "", // TODO
+    "pois", // suomi
+    "", // TODO
+    "Desactivar", // Español
+    "", // TODO
+    "", // TODO  
+    "", // TODO   
+    "", // TODO
+    "", // TODO   
+#if VDRVERSNUM >= 10302  
+    "", // TODO               
+#if VDRVERSNUM >= 10307  
+    "", // TODO                 
+#endif  
+#endif  
+  },
+  { NULL }
+};
+
+#endif // __GAMES_I18N_H
