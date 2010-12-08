37e24f8905becc1a82b7465db4827dbd6dffaa0d
bittor <76845083+bittor7x0@users.noreply.github.com>
2010-12-08 11:28:28 +0000

    Adaptar plugin osdadjust-0.0.5 para compilar en M7x0 y añadir traducción al español.

---
+++ vdr-m7x0-plugins/osdadjust-0.0.5/i18n.c
@@ -0,0 +1,15 @@
+/*
+ * i18n.c: Internationalization
+ *
+ * See the README file for copyright information and how to reach the author.
+ *
+ */
+
+#include <vdr/plugin.h>
+
+#if APIVERSNUM < 10507
+
+#include "i18n.h"
+#include "i18n-generated.h"
+
+#endif
