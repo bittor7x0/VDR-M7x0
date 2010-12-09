56e09f1a45df2db55ce561a5636a992734c43694
bittor <bittor@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-12-09 10:38:31 +0000

    Parche para VDR: 360-vdr-submenu-0.7-1.3.38+.patch
    ------------------------------------------------------------------------
    r413 | bittor | 2009-01-18 12:01:01 +0100 (dom 18 de ene de 2009)
    
    Ticket #64 - Menú principal sencillo y configurable.
    También se pueden recrear los submenús del menú principal desde el commands.conf (por si queremos hacerlo cuando activamos/desactivamos plugins).
    ------------------------------------------------------------------------

---
+++ trunk/vdr-m7x0/submenu.h
@@ -0,0 +1,62 @@
+#ifndef __SUBMENU_H
+#define __SUBMENU_H
+
+#define SIZEMAXNAMESUBMENUITEMINFO 30
+#define NBMAXSUBMENUANDITEM 255
+
+class cSubMenu;
+
+class cSubMenuItemInfo
+{
+	private:
+	public:
+		cSubMenuItemInfo();
+		~cSubMenuItemInfo();
+
+		int menuLevel;
+		int kindOfItem;
+		char name[SIZEMAXNAMESUBMENUITEMINFO];  //todo revoir ca(la taille en dur)
+
+		void setname(const char *n);
+};
+
+class cArbo
+{
+	private:
+	public:
+	      cArbo(void);
+	      ~cArbo(void);
+	      int Index[NBMAXSUBMENUANDITEM][NBMAXSUBMENUANDITEM];
+	      int CurrentIndex[NBMAXSUBMENUANDITEM];
+	      int MaxLevel;
+	      int MaxItem[NBMAXSUBMENUANDITEM];
+	      void Populate(cSubMenu *submenu);
+              void Clear(void);
+};
+
+class cSubMenu
+{
+	private:
+	public:
+		int currentMenuLevel;
+		cSubMenuItemInfo *subMenuItem;
+		cArbo Arbo;
+		int nbSubMenuItem;
+		int oldNumberMenu[ NBMAXSUBMENUANDITEM ];
+		int nbLevelMenu;
+
+		int offset; // For MenuCmdPosition issue
+
+		cSubMenu();
+		virtual ~cSubMenu();
+		void LoadInfoSubMenu(void);
+		void ParseOneLigOfFile (const char *lig);
+		void SaveInfoSubMenu(void);
+		int GetByLevel(int Level, int curr);
+		int GetByLevel(int Level);
+	        void CreateItem(int type,int position,int where,char *Name="");
+	        void DeleteItem(int position,int kindItem=-1);
+		void InsertItem(int position);
+		void Dump(void);
+};
+#endif //__SUBMENU_H
