0c99038e00b4c965ecfdf41794b52d0dfeeddeed
bittor <76845083+bittor7x0@users.noreply.github.com>
2011-07-12 18:19:22 +0000

    Corregir url de descarga de vsftpd (gracias a Posix por el aviso).
    URL con información del problema:
    http://scarybeastsecurity.blogspot.com/2011/07/alert-vsftpd-download-backdoored.html

--- toolchain/make-incs/vsftpd.mk
+++ toolchain/make-incs/vsftpd.mk
@@ -22,7 +22,7 @@ VSFTPD_PATCHES_DIR := $(PATCHES_DIR)/vsftpd/$(VSFTPD_VERSION)
 
 VSFTPD_FILE := vsftpd-$(VSFTPD_VERSION).tar.gz
 VSFTPD_DLFILE := $(DOWNLOAD_DIR)/$(VSFTPD_FILE)
-VSFTPD_URL := ftp://vsftpd.beasts.org/users/cevans/$(VSFTPD_FILE)
+VSFTPD_URL := https://security.appspot.com/downloads/$(VSFTPD_FILE)
 VSFTPD_DIR := $(BUILD_DIR)/vsftpd-$(VSFTPD_VERSION)
 
 VSFTPD_INSTALLED = $(STAGEFILES_DIR)/.vsftpd_installed
