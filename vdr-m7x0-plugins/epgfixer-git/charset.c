9c6612493019b8b2c286944916c054fc1f4f3d61
bittor <76845083+bittor7x0@users.noreply.github.com>
2016-07-14 19:47:16 +0000

    Correcciones para el plugin epgfixer

--- vdr-m7x0-plugins/epgfixer-git/charset.c
+++ vdr-m7x0-plugins/epgfixer-git/charset.c
@@ -7,30 +7,7 @@
 
 #include <string.h>
 #include "charset.h"
-
-#if APIVERSNUM < 10500
-class cCharSetConv {
-private:
-  static char *systemCharacterTable;
-public:
-  cCharSetConv(const char *FromCode = NULL, const char *ToCode = NULL);
-     ///< Sets up a character set converter to convert from FromCode to ToCode.
-     ///< If FromCode is NULL, the previously set systemCharacterTable is used.
-     ///< If ToCode is NULL, "UTF-8" is used.
-  ~cCharSetConv();
-  const char *Convert(const char *From, char *To = NULL, size_t ToLength = 0);
-     ///< Converts the given Text from FromCode to ToCode (as set in the cosntructor).
-     ///< If To is given, it is used to copy at most ToLength bytes of the result
-     ///< (including the terminating 0) into that buffer. If To is not given,
-     ///< the result is copied into a dynamically allocated buffer and is valid as
-     ///< long as this object lives, or until the next call to Convert(). The
-     ///< return value always points to the result if the conversion was successful
-     ///< (even if a fixed size To buffer was given and the result didn't fit into
-     ///< it). If the string could not be converted, the result points to the
-     ///< original From string.
-  static const char *SystemCharacterTable(void) { return systemCharacterTable; }
-  };
-#endif // APIVERSNUM < 10500
+#include "compatibility.h"
 
 /* Global instance */
 cEpgfixerList<cCharSet, cEvent> EpgfixerCharSets;
