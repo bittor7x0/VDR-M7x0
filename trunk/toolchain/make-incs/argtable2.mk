6591f58cbacacd445039595adf0a0b16ac2d8f13
bittor <bittor@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2016-07-16 10:49:06 +0000

    Añadir comskip (detecta anuncios en grabaciones) y librería argtable2 del que depende

---
+++ trunk/toolchain/make-incs/argtable2.mk
@@ -0,0 +1,126 @@
+# --- VDR-NG-EM-COPYRIGHT-NOTE-BEGIN ---
+#
+# Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 VDR-NG-EM Project
+#
+# More information can be found in the files COPYING and README.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# The project's page is at http://www.assembla.com/spaces/VDR-M7x0
+# Our English forum is at  http://vdr-m7x0.foroactivo.com.es/f19-international-forum
+# Our Spanish forum is at  http://vdr-m7x0.foroactivo.com.es
+#
+# --- VDR-NG-EM-COPYRIGHT-NOTE-END ---
+
+# Put dependencies here all pack should depend on $$(BASE_BUILD_STAGEFILE)
+ARGTABLE2_DEPS = $(BASE_BUILD_STAGEFILE)
+
+ARGTABLE2_VERSION := 2-13
+ARGTABLE2_VERSION_POINT := 2.13
+ARGTABLE2_PATCHES_DIR := $(PATCHES_DIR)/argtable2/$(ARGTABLE2_VERSION_POINT)
+
+ARGTABLE2_FILE := argtable$(ARGTABLE2_VERSION).tar.gz
+ARGTABLE2_DLFILE := $(DOWNLOAD_DIR)/$(ARGTABLE2_FILE)
+ARGTABLE2_URL := https://sourceforge.net/projects/argtable/files/argtable/argtable-$(ARGTABLE2_VERSION_POINT)/$(ARGTABLE2_FILE)/download
+ARGTABLE2_DIR := $(BUILD_DIR)/argtable$(ARGTABLE2_VERSION)
+
+ARGTABLE2_INSTALLED = $(STAGEFILES_DIR)/.argtable2_installed
+
+PACKS_RULES_$(CONFIG_ARGTABLE2) += $(ARGTABLE2_INSTALLED)
+FILE_LISTS_$(CONFIG_ARGTABLE2) += argtable2.lst
+
+CLEAN_RULES += clean-argtable2
+DISTCLEAN_RULES += distclean-argtable2
+
+#
+# download argtable2
+#
+
+$(ARGTABLE2_DLFILE): $(TC_INIT_RULE)
+	(if [ ! -f $(ARGTABLE2_DLFILE) ] ; then \
+	$(WGET) $(ARGTABLE2_URL) -O $(ARGTABLE2_DLFILE) ; \
+	fi );
+	$(TOUCH) $(ARGTABLE2_DLFILE)
+
+#
+# unpack argtable2
+#
+
+$(STAGEFILES_DIR)/.argtable2_unpacked: $(ARGTABLE2_DLFILE) \
+                                           $(wildcard $(ARGTABLE2_PATCHES_DIR)/*.patch) \
+                                           $$(ARGTABLE2_DEPS)
+	-$(RM) -rf $(ARGTABLE2_DIR)
+	$(TAR) -C $(BUILD_DIR) -zf $(ARGTABLE2_DLFILE)
+	$(TOUCH) $(STAGEFILES_DIR)/.argtable2_unpacked
+
+#
+# patch argtable2
+#
+
+$(STAGEFILES_DIR)/.argtable2_patched: $(STAGEFILES_DIR)/.argtable2_unpacked
+	$(call patch_package, $(ARGTABLE2_DIR), $(ARGTABLE2_PATCHES_DIR))
+	$(TOUCH) $(STAGEFILES_DIR)/.argtable2_patched
+
+#
+# configure argtable2
+#
+
+$(STAGEFILES_DIR)/.argtable2_configured: $(STAGEFILES_DIR)/.argtable2_patched
+	($(CD) $(ARGTABLE2_DIR) ; $(UCLIBC_ENV_GC) \
+		CFLAGS="$(UCLIBC_CFLAGS_GC) -fPIC" \
+		$(ARGTABLE2_DIR)/configure \
+			--prefix=$(TARGET_ROOT)/usr \
+			--host=$(TARGET) \
+			--enable-shared \
+			--enable-static)
+	$(TOUCH) $(STAGEFILES_DIR)/.argtable2_configured
+
+#
+# compile argtable2
+#
+
+$(STAGEFILES_DIR)/.argtable2_compiled: $(STAGEFILES_DIR)/.argtable2_configured
+	$(MAKE) -C $(ARGTABLE2_DIR) $(UCLIBC_ENV_GC)
+	$(TOUCH) $(STAGEFILES_DIR)/.argtable2_compiled
+
+#
+# install argtable2
+#
+
+$(STAGEFILES_DIR)/.argtable2_installed: $(STAGEFILES_DIR)/.argtable2_compiled
+	$(MAKE) -C $(ARGTABLE2_DIR) $(UCLIBC_ENV_GC) install
+	$(TOUCH) $(STAGEFILES_DIR)/.argtable2_installed
+
+
+$(FILELIST_DIR)/argtable2.lst: $(STAGEFILES_DIR)/.argtable2_installed
+	$(TOUCH) $(FILELIST_DIR)/argtable2.lst
+
+.PHONY: clean-argtable2 distclean-argtable2
+
+clean-argtable2:
+	-$(RM) -rf $(ARGTABLE2_DIR)
+
+#
+# clean everthing else
+#
+
+distclean-argtable2:
+	-$(RM) -f $(STAGEFILES_DIR)/.argtable2_unpacked
+	-$(RM) -f $(STAGEFILES_DIR)/.argtable2_patched
+	-$(RM) -f $(STAGEFILES_DIR)/.argtable2_configured
+	-$(RM) -f $(STAGEFILES_DIR)/.argtable2_compiled
+	-$(RM) -f $(STAGEFILES_DIR)/.argtable2_installed
+ifeq ($(DISTCLEAN_DLFILE),y)
+	-$(RM) -rf $(ARGTABLE2_DLFILE)
+endif
---
+++ trunk/toolchain/make-incs/comskip.mk
@@ -0,0 +1,133 @@
+# --- VDR-NG-EM-COPYRIGHT-NOTE-BEGIN ---
+#
+# Copyright (C) 2016 VDR-NG-EM Project
+#
+# More information can be found in the files COPYING and README.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# The project's page is at http://www.assembla.com/spaces/VDR-M7x0
+# Our English forum is at  http://vdr-m7x0.foroactivo.com.es/f19-international-forum
+# Our Spanish forum is at  http://vdr-m7x0.foroactivo.com.es
+#
+# --- VDR-NG-EM-COPYRIGHT-NOTE-END ---
+
+ifeq ($(CONFIG_COMSKIP),y)
+ifneq ($(and $(filter y,$(CONFIG_ARGTABLE2)),$(filter y,$(CONFIG_FFMPEG)),$(filter y,$(CONFIG_FFMPEG_LIBAVCODEC)),$(filter y,$(CONFIG_FFMPEG_LIBAVUTIL)),$(filter y,$(CONFIG_FFMPEG_LIBAVFORMAT))),y)
+   $(error dependency error: comskip needs argtable2 and ffmpeg with libavcodec, libavutil and libavformat libraries enabled)
+endif
+endif 
+
+# Put dependencies here all pack should depend on $$(BASE_BUILD_STAGEFILE)
+COMSKIP_DEPS = $(BASE_BUILD_STAGEFILE) $(ARGTABLE2_INSTALLED) $(FFMPEG_INSTALLED)
+
+COMSKIP_VERSION := 53765ee88418cee90f22e4232e6af06e110c78e9
+COMSKIP_PATCHES_DIR := $(PATCHES_DIR)/comskip/snapshot
+
+COMSKIP_FILE := Comskip-$(COMSKIP_VERSION).zip
+COMSKIP_DLFILE := $(DOWNLOAD_DIR)/$(COMSKIP_FILE)
+COMSKIP_URL := https://github.com/erikkaashoek/Comskip/archive/$(COMSKIP_VERSION).zip
+COMSKIP_DIR := $(BUILD_DIR)/Comskip-$(COMSKIP_VERSION)
+
+COMSKIP_INSTALLED = $(STAGEFILES_DIR)/.comskip_installed
+
+PACKS_RULES_$(CONFIG_COMSKIP) += $(COMSKIP_INSTALLED)
+FILE_LISTS_$(CONFIG_COMSKIP) += comskip.lst
+
+CLEAN_RULES += clean-comskip
+DISTCLEAN_RULES += distclean-comskip
+
+#
+# download comskip
+#
+
+$(COMSKIP_DLFILE): $(TC_INIT_RULE)
+	(if [ ! -f $(COMSKIP_DLFILE) ] ; then \
+	$(WGET) $(COMSKIP_URL) -O $(COMSKIP_DLFILE) ; \
+	fi );
+	$(TOUCH) $(COMSKIP_DLFILE)
+
+#
+# unpack comskip
+#
+
+$(STAGEFILES_DIR)/.comskip_unpacked: $(COMSKIP_DLFILE) \
+                                           $(wildcard $(COMSKIP_PATCHES_DIR)/*.patch) \
+                                           $$(COMSKIP_DEPS)
+	-$(RM) -rf $(COMSKIP_DIR)
+	(cd $(BUILD_DIR) ; $(UNZIP) -x $(COMSKIP_DLFILE))
+	$(TOUCH) $(STAGEFILES_DIR)/.comskip_unpacked
+
+#
+# patch comskip
+#
+
+$(STAGEFILES_DIR)/.comskip_patched: $(STAGEFILES_DIR)/.comskip_unpacked
+	$(call patch_package, $(COMSKIP_DIR), $(COMSKIP_PATCHES_DIR))
+	$(TOUCH) $(STAGEFILES_DIR)/.comskip_patched
+
+#
+# configure comskip
+#
+
+$(STAGEFILES_DIR)/.comskip_configured: $(STAGEFILES_DIR)/.comskip_patched
+	($(CD) $(COMSKIP_DIR) ; $(COMSKIP_DIR)/autogen.sh ; \
+		$(UCLIBC_ENV_LTO_GC) \
+		LDFLAGS="$(UCLIBC_LDFLAGS_LTO) -L$(TARGET_ROOT)/lib -L$(TARGET_ROOT)/usr/lib -Wl,-rpath-link=$(TARGET_ROOT)/usr/lib" \
+		PKG_CONFIG_PATH="$(TARGET_ROOT)/usr/lib/pkgconfig" \
+		PKG_CONFIG_LIBDIR="$(TARGET_ROOT)/usr/lib/pkgconfig" \
+		$(COMSKIP_DIR)/configure \
+			--prefix=$(TARGET_ROOT)/usr \
+			--host=$(TARGET) \
+			--enable-donator)
+	$(TOUCH) $(STAGEFILES_DIR)/.comskip_configured
+
+#
+# compile comskip
+#
+
+$(STAGEFILES_DIR)/.comskip_compiled: $(STAGEFILES_DIR)/.comskip_configured
+	$(UCLIBC_ENV_LTO_GC) $(MAKE) -C $(COMSKIP_DIR)
+	$(TOUCH) $(STAGEFILES_DIR)/.comskip_compiled
+
+#
+# pack comskip
+#
+
+$(STAGEFILES_DIR)/.comskip_installed: $(STAGEFILES_DIR)/.comskip_compiled
+	$(CP) $(COMSKIP_DIR)/comskip $(TARGET_ROOT)/usr/bin/comskip
+	$(TOUCH) $(STAGEFILES_DIR)/.comskip_installed
+
+
+$(FILELIST_DIR)/comskip.lst: $(STAGEFILES_DIR)/.comskip_installed
+	$(TOUCH) $(FILELIST_DIR)/comskip.lst
+
+.PHONY: clean-comskip distclean-comskip
+
+clean-comskip:
+	-$(RM) -rf $(COMSKIP_DIR)
+
+#
+# clean everthing else
+#
+
+distclean-comskip:
+	-$(RM) -f $(STAGEFILES_DIR)/.comskip_unpacked
+	-$(RM) -f $(STAGEFILES_DIR)/.comskip_patched
+	-$(RM) -f $(STAGEFILES_DIR)/.comskip_configured
+	-$(RM) -f $(STAGEFILES_DIR)/.comskip_compiled
+	-$(RM) -f $(STAGEFILES_DIR)/.comskip_installed
+ifeq ($(DISTCLEAN_DLFILE),y)
+	-$(RM) -rf $(COMSKIP_DLFILE)
+endif
