af20ee3697a73ca7969e2faf5c9547bb71b96fde
bittor <bittor@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2018-07-07 09:12:48 +0000

    Actualizar copyright.

--- trunk/toolchain/make-incs/smartmontools.mk
+++ trunk/toolchain/make-incs/smartmontools.mk
@@ -1,6 +1,6 @@
 # --- VDR-NG-EM-COPYRIGHT-NOTE-BEGIN ---
 #
-# Copyright (C) 2013 VDR-NG-EM Project
+# Copyright (C) 2013-2018 VDR-NG-EM Project
 #
 # More information can be found in the files COPYING and README.
 #
