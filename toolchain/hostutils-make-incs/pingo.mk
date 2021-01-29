77a3ee63b5bbda1dfa57cacd2450d382f4700784
bittor7x0 <76845083+bittor7x0@users.noreply.github.com>
2021-01-29 17:28:17 +0100

    Corregir descarga de pingo

--- toolchain/hostutils-make-incs/pingo.mk
+++ toolchain/hostutils-make-incs/pingo.mk
@@ -29,7 +29,7 @@ PINGO_HOSTVERSION := 0.99.beta63
 PINGO_HOSTFILE := pingo-nix64.zip
 PINGO_HOSTDLFILE := $(DOWNLOAD_DIR)/$(PINGO_HOSTFILE)
 
-PINGO_HOSTURL := https://css-ig.net/bin/$(PINGO_HOSTFILE)
+PINGO_HOSTURL := https://raw.githubusercontent.com/bittor7x0/VDR-M7x0/master/toolchain/downloads/$(PINGO_HOSTFILE)
 PINGO_HOSTDIR := $(HOSTUTILS_BUILD_DIR)/pingo-$(PINGO_HOSTVERSION)
 
 CLEAN_RULES += clean-pingo-host
@@ -41,15 +41,7 @@ DISTCLEAN_RULES += distclean-pingo-host
 
 $(PINGO_HOSTDLFILE): $(TC_INIT_RULE)
 	(if [ ! -f $(PINGO_HOSTDLFILE) ] ; then \
-	$(WGET) $(PINGO_HOSTURL) -O $(PINGO_HOSTDLFILE) \
-		--header='authority: css-ig.net' \
-		--header='upgrade-insecure-requests: 1' \
-		--header='User-Agent: Mozilla/5.0 (X11; U; Linux x86_64; de; rv:1.9.2.8) Gecko/20100723 Ubuntu/10.04 (lucid) Firefox/3.6.8' \
-		--header='Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' \
-		--header='Referer: https://css-ig.net/pingo' \
-		--header='Accept-Encoding: gzip, deflate, br' \
-		--header='Accept-Language: en-US,en;q=0.9,pt;q=0.8' \
-		--header='Cookie: HttpOnly; SERVERID105614=1420122|XSyhV|XSyhV' ; \
+	$(WGET) $(PINGO_HOSTURL) -O $(PINGO_HOSTDLFILE) ; \
 	fi );
 	$(TOUCH) $(PINGO_HOSTDLFILE)
 
